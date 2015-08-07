ac7d8654a544fbc004eac015a08bb58d9d630a36
Connorelsea <connorelsea@gmail.com>
2015-08-07 07:48:26 -0700

    Adding working parallax and styling news section

--- public/scripts/animationHeader.js
+++ public/scripts/animationHeader.js
@@ -12,22 +12,28 @@ var classes = [
 var widthCorrection = function() {
 
 	var width    = $(window).width()
-	var newWidth = width + 200
+	var newWidth = width + 400
 
 	$(".primaryCover").css({ "width" : newWidth })
 	$(".secondaryCover").css({ "width" : newWidth })
+	$(".tertiaryCover").css({ "width" : newWidth })
 
 }
 
 $(window).resize(widthCorrection);
 $(document).ready(widthCorrection);
 
+var topWidthNormal = 180;
+var topWidthSmall  = 80;
+
 $(window).scroll(function() {
 
 	// If the user has scrolled down, change the
 	// main bar's class, making it smaller.
 
-	if ($(document).scrollTop() > 0) {
+	var scroll = $(document).scrollTop();
+
+	if (scroll > 0) {
 
 		for (i = 0; i < classes.length; ++i) {
 			$(classes[i].parent).addClass(classes[i].addon)
@@ -40,5 +46,40 @@ $(window).scroll(function() {
 		}
 
 	}
+
+	// Apply parallax effects for the covers
+
+	var primaryScroll = scroll / 50;
+	var primaryRotate = -4 + (scroll / 100);
+
+	$(".primaryCover").css({
+		"transform" : "translate(0px, -" + primaryScroll + "%) rotate(" + primaryRotate + "deg)"
+	})
+	
+	var secondaryScroll = scroll / 10;
+	var secondaryRotate = -1 + (scroll / -100)
+
+	$(".secondaryCover").css({
+		"transform" : "translate(0px, -" + secondaryScroll + "%) rotate(" + secondaryRotate + "deg)"
+	})
+
+	var foregroundScroll = topWidthNormal - (scroll / 5)
+
+	$(".foreground").css({
+		"top" : foregroundScroll
+	})
+
+	var tertiaryScroll = scroll / 200;
+	var tertiaryRotate = -1.5 + (scroll / -200)
+
+	$(".tertiaryCover").css({
+		"transform" : "translate(0px, -" + tertiaryScroll + "%) rotate(" + tertiaryRotate + "deg)"
+	})
+
+	var actionScale = 1 - (scroll / 100)
+
+	$(".innerBar .action").css({
+		"transform" : "scale(" + actionScale + "%)"
+	})
 
 });
